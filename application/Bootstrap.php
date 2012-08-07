84f007863dc0d8cedea87e0195e558680c8c00db
daniel <daniel@deny.(none)>
2012-08-07 21:20:12 +0200

    + dodany ScaZF i konfiguracja wstępna

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,5 +5,31 @@
  */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-	
+	/**
+	 * Ustawienia autoloadera
+	 */
+	protected function _initAutoload()
+	{
+		Zend_Loader_Autoloader::getInstance()
+			->setFallbackAutoloader(true);
+	}
+
+	/**
+	 * Przerzucanie resource do rejestru
+	 */
+	protected function _initResources()
+	{
+		// baza danych
+		$this->_bootstrap('db');
+		Zend_Registry::set('db', $this->getResource('db'));
+	}
+
+	/**
+	 * Init ScaZF options
+	 */
+	public function _initScaZF()
+	{
+		\Sca\Config::getInstance()->setDbAdapter($this->getResource('db'))
+									->setModelAutoload(APPLICATION_PATH . '/models');
+	}
 }
